ecb74fdeb623c9a0f9aaed6d697e99b80cabe897
Ani <anibhagwat17@hmail.com>
2022-07-07 19:50:05 +0530

    added rightNav

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import {Navbar} from './Components/Navbar';
+import {Navbar} from './Components/Navbar'; 
+import rightNav from './Components/rightNav';
 function App() {
   return (
     <>
-      <Navbar title="Blog Website"/>
+      {/*<Navbar title="Blog Website"/>*/}
+      hbfjdf
+      <rightNav />
     </>
   );
 }
---
+++ my-app/src/Components/rightNav.js
@@ -0,0 +1,8 @@
+function rightNav() {
+    return(
+        <div className="rightNav">
+            <h1> hhaha ahahaha </h1>
+        </div>
+    );
+}
+export default rightNav;

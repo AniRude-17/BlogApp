ad7ed4dfc3dc2351c96a8f879e315f39e0bcd0db
anir-ab <anibhagwat17@gmail.com>
2022-07-09 16:42:47 +0530

    removed errors

--- my-app/src/Components/RightNav.js
+++ my-app/src/Components/RightNav.js
@@ -1,11 +1,8 @@
 import '../css/RightNav.css'
 import Searchbox from './Rightnav-Components/Searchbox';
 import Tagcloud from './Rightnav-Components/Tagcloud';
-<<<<<<< HEAD
 import { Blogprev } from './Blogprev';
 import Catagories from './Rightnav-Components/Catagories';
-=======
->>>>>>> e1c2b5aa9589e6e5730188fa1c2fd3d73a1942b2
 
 export const RightNav = () => {
     return (
@@ -14,13 +11,9 @@ export const RightNav = () => {
                 <Searchbox />
                 <h3 className='RightNav-headings'> Tag Cloud </h3>
                 <Tagcloud />
-<<<<<<< HEAD
                 <h3 className='RightNav-headings'> Categories </h3>
-                <Catagories />
-                
-=======
+                <Catagories />        
                 <h3 className='RightNav-headings'> Categories </h3>'
->>>>>>> e1c2b5aa9589e6e5730188fa1c2fd3d73a1942b2
             </div>
         </div>
     );

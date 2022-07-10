08c39a6a1a71b73e633f84e1ce66eb7f545d744e
AdityaHanda <aditya.handa001@gmail.com>
2022-07-11 02:57:25 +0530

    Popart: Props value access error removed

--- my-app/src/Components/Rightnav-Components/Popart.js
+++ my-app/src/Components/Rightnav-Components/Popart.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import {Rightnav_blogprev} from './Rightnav_blogprev'
-let props=[{ headline:"My First Article!!" , content:"abcd efgh ijkl mnop qrst", date:"12/7/2020" , writer:"Jon Snow"}, { headline:"My First Article!!" , content:"abcd efgh ijkl mnop qrst", date:"12/7/2020" , writer:"Jon Snow"}, { headline:"My First Article!!" , content:"abcd efgh ijkl mnop qrst", date:"12/7/2020" , writer:"Jon Snow"} ]
+let props=[{ headline:"My First Article!!" , content:"The Great Blue Planet", date:"12/7/2020" , writer:"Jon Snow"},{headline:"My second Article!!" , content:"Time as we know, a uni.....", date:"12/7/2020" , writer:"Jon Snow"}  ]
+
 export const Popart = () => {
+    
     return (
         
+
         <div className="Popart-box">
             
-            {props.map((article) => {
-            return(
-                <Rightnav_blogprev
-                    props={article}
-                />
-                
-                
-            )
+            {props.map((article) => (
+                    
+                    <Rightnav_blogprev
+                        props={article}
+
+                    />
             
-        })}
+
+            ))}
+
         </div>
     );
+    
     
 }
--- my-app/src/Components/Rightnav-Components/Rightnav_blogprev.jsx
+++ my-app/src/Components/Rightnav-Components/Rightnav_blogprev.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "../../css/RightNav.css"
+
 export const Rightnav_blogprev = (props) => {
+
     return(
         <div className="Blogbox">
+            
 { /* src={props.thumbnail} : for final development !!  */}
             <img className="Thumbnail-image" src="http://unsplash.it/250/250?gravity=center" />
-            <div className="blog-headline-box">{props.headline}</div>
-            <div className="blog-content-box">{props.content}</div>
+            <div className="blog-headline-box">{props.props.headline}</div>
+            <div className="blog-content-box">{props.props.content}</div>
             <div className="blog-extrainfo-box">
                 <div className="date-posted-box">
                     {/* <image className="calander-icon" src="http://unsplash.it/250/250?gravity=center" /> */}
@@ -14,12 +17,15 @@ export const Rightnav_blogprev = (props) => {
                 </div>
                 <div className="written-by-box">
                 {/* <image className="person-icon" src="http://unsplash.it/250/250?gravity=center" /> */}
-                    <p className="writer">{props.writer}</p>
+                    <p className="writer">{props.props.writer}</p>
                 </div>
             </div>
+            
         </div>
+
             
             
     )
 };
+
 

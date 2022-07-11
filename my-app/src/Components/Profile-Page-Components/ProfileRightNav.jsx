0ceb7522165379fd88dc9e29685bef571ddc11cb
Prashant <prashantk2208@gmail.com>
2022-07-12 00:23:19 +0530

    Profile Navbar

--- my-app/src/Components/Profile-Page-Components/ProfileRightNav.jsx
+++ my-app/src/Components/Profile-Page-Components/ProfileRightNav.jsx
@@ -6,6 +6,13 @@ export const ProfileRightNav = () => {
     <div className="RightNav">
             <div className='RightComponent'>    
                 <Searchbox />
+                <div className="edit">
+                    <a href="">Edit Profile</a>
+                </div>
+                <div id="follownav">
+                <a href="" className='nav-link'>Following</a>
+                <a href="" className='nav-link'>Followers</a> 
+                </div>
             </div>
         </div>
   )

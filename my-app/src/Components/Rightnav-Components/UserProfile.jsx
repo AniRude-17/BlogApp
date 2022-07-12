c54b9f5528ffeab6ee5ea24f77365e067ed9b63f
anir-ab <anibhagwat17@gmail.com>
2022-07-12 11:57:12 +0530

    minor edits in userProfile

--- my-app/src/Components/Rightnav-Components/UserProfile.jsx
+++ my-app/src/Components/Rightnav-Components/UserProfile.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import '../../css/profile.css'
 
 export const UserProfile = () => {
+  let userName='Blog User'
+  let title = "A Loving Heart is the Truest Wisdom";
+  let datep = "July 9, 2022";
+  let tag = "Travel";
+  let num = "50";
+
   return (
     <div className='UserProfile'>
         <img className='ProfilePic' src='http://unsplash.it/250/250?gravity=center' />
-        <h2> Blog User</h2>
-        <h4> A quick brown fox jumps over the lazy dog </h4>
+        <h2> {userName} </h2>
+        <p> The quick brown fox jumps over a lazy dog </p>
+
+        <h4> {userName +'\'s'} Blogs </h4>
+        
     </div>
   )
 }

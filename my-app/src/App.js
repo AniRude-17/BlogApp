70cedafffd77e4210389df0abd79aff6d0906a01
anir-ab <anibhagwat17@gmail.com>
2022-07-12 11:38:37 +0530

    corrected RightNav vh and Popart

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -40,6 +40,7 @@ function App() {
           return(
             <>
               {/* <BlogForm/> */}
+              <UserProfile></UserProfile>
               <ProfileRightNav/>  
             </>
           )
--- my-app/src/Components/Rightnav-Components/UserProfile.jsx
+++ my-app/src/Components/Rightnav-Components/UserProfile.jsx
@@ -4,7 +4,9 @@ import '../../css/profile.css'
 export const UserProfile = () => {
   return (
     <div className='UserProfile'>
-        <h1> Profile </h1>
+        <img className='ProfilePic' src='http://unsplash.it/250/250?gravity=center' />
+        <h2> Blog User</h2>
+        <h4> A quick brown fox jumps over the lazy dog </h4>
     </div>
   )
 }

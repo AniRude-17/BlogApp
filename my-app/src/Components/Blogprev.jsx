c7b7fcc9ffdd6a59951601f00899c25e1bd892ee
Moriarty2610 <kushal261002@gmail.com>
2022-07-14 20:46:08 +0530

    more map changes

--- my-app/src/Components/Blogprev.jsx
+++ my-app/src/Components/Blogprev.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "../css/blogpreview.css";
-let props=[{title:"A Loving Heart is the Truest Wisdom", datep:"July 9, 2022", tag:"Travel", num:"50", content:"A small river named Duden flows by their place and supplies it with the necessary regelialia."},
-          {title:"Great Things Never Came from Comfort Zone", datep:"June 28, 2019", tag:"Travel", num:"69",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia."}]
+let props=[{title:"A Loving Heart is the Truest Wisdom", datep:"July 9, 2022", tag:"Travel", num:"50", content:"A small river named Duden flows by their place and supplies it with the necessary regelialia.",img:"http://unsplash.it/250/250?gravity=center"},
+          {title:"Great Things Never Came from Comfort Zone", datep:"June 28, 2019", tag:"Travel", num:"69",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia.",img:"http://unsplash.it/250/250?gravity=center"},
+          {title:"Great Things Never Came from Comfort Zone", datep:"June 28, 2019", tag:"Travel", num:"69",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia.",img:"http://unsplash.it/250/250?gravity=center"},
+          {title:"Great Things Never Came from Comfort Zone", datep:"June 28, 2019", tag:"Travel", num:"69",content:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia.",img:"http://unsplash.it/250/250?gravity=center"}]
 export const Blogprev = () => {
   return (
     <div class="prev">
@@ -9,7 +11,7 @@ export const Blogprev = () => {
                     <div class="blog" data-aos="fade-up">
                     <img
                       className="thumbnail-image"
-                      src="http://unsplash.it/250/250?gravity=center"
+                      src={contents.img}
                     />
                     <div className="blog-prev-contents">
                       <h3 className="head3">

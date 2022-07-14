4845be76411676198be97f1a33690f25d042e591
AdityaHanda <aditya.handa001@gmail.com>
2022-07-15 01:42:25 +0530

    Readmore Rightnav work started

--- my-app/src/Components/Rightnav-Components/Popart.js
+++ my-app/src/Components/Rightnav-Components/Popart.js
@@ -13,7 +13,7 @@ export const Popart = () => {
                     
                     <Rightnav_blogprev
                         props={article}
-
+                         
                     />
             
 

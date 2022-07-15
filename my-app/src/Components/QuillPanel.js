dbd9bb5b82f503a13642f8f9bc55afabed4ca567
anir-ab <anibhagwat17@gmail.com>
2022-07-15 16:20:40 +0530

    some quill panel changes

--- my-app/src/Components/QuillPanel.js
+++ my-app/src/Components/QuillPanel.js
@@ -2,22 +2,16 @@ import React from "react";
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-class QuillPanel extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = { text: '' } // You can also pass a Quill Delta here
-      this.handleChange = this.handleChange.bind(this)
-    }
-   
-    handleChange(value) {
-      this.setState({ text: value })
-    }
-   
-    render() {
-      return (
-        <ReactQuill value={this.state.text}
-                    onChange={this.handleChange} />
-      )
-    }
-  }
-  export default QuillPanel;
+function QuillPanel () {
+
+
+  return(
+
+    <div>
+      <ReactQuill placeholder="Start your Story..."/>
+    </div>
+  );
+
+}
+
+export default QuillPanel;

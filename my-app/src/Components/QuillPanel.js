4e07a7f8c4dedccd1eeec75ceac54992ec158bd9
anir-ab <anibhagwat17@gmail.com>
2022-07-15 16:43:46 +0530

    Linked Read More

--- my-app/src/Components/QuillPanel.js
+++ my-app/src/Components/QuillPanel.js
@@ -1,17 +1,28 @@
 import React from "react";
-import ReactQuill, { Quill } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
+ import ReactQuill from 'react-quill';
+ import 'react-quill/dist/quill.snow.css';
 
-function QuillPanel () {
+ function QuillPanel() {
+  const modules = {
+    toolbar: [
+      [{ size: [] }],
+      ['bold', 'italic', 'underline'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+      ],
+      ['link', 'image', 'video'],
+      ['clean'],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  }
 
+   return (
+     <ReactQuill theme="snow" modules={modules}></ReactQuill>
+   );
+ }
 
-  return(
-
-    <div>
-      <ReactQuill placeholder="Start your Story..."/>
-    </div>
-  );
-
-}
-
-export default QuillPanel;
+ export default QuillPanel;
